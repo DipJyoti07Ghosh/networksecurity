7a25a4b5b6334321616e58647b6fffeaf80df592
Dipjyoti Ghosh <dipjyotighosh00@gmail.com>
2026-07-28 22:17:23 +0530

    Setuo.py file develop

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requiremets()->List[str]:
+  requirement_lst:List[str]=[]
+  try:
+    with open('requirements.txt','r') as file:
+      lines=file.readlines()
+      for line in lines:
+        requirement=line.strip()
+        if requirement and requirement!='-e .':
+          requirement_lst.append(requirement)
+  except FileNotFoundError:
+    print("requirements.txt not found")
+
+  return requirement_lst
+
+setup(
+  name='NetworkSecurity',
+  version="0.0.1",
+  author="Dipjyoti Ghosh",
+  author_email="dipjyotighosh00@gmail.com",
+  packages=find_packages(),
+  install_requires=get_requiremets()
+)
+  
